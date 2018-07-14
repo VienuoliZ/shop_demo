199655bafb8ddc4c42fe64508ffb11028f59ad10
Aurelijus <aurelijus@gmail.com>
2018-07-14 12:01:38 +0300

    second commit to stage-1

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import requests
 
 # Выведет HTML страницы моего курса
-result = requests.get("https://api.cryptonator.com/api/ticker/btc-usd")
-print(result.text)
+url = "https://api.telegram.org/bot684387669:AAGLlY7kaiNPKQaEWl1a99M4yMD4PgaAYmg/getMe"
+
+response = requests.get(url)
+
+# форматируем json в словарь
+decoded = response.json()
+print(decoded)
+
+bot_result = decoded['result']
+bot_id = bot_result['id']
+
+# также можем сделать это сразу
+bot_id = decoded['result']['id']
